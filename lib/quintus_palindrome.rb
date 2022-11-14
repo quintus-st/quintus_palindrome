5662fdf88c16be6e904fc76398b38d381a2672a0
quintus-st <quintus.flannagan@simplethread.com>
2022-11-14 13:43:47 -0500

    init commit

---
+++ lib/quintus_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quintus_palindrome/version"
+
+module QuintusPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_quintus_palindrome.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestQuintusPalindrome < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::QuintusPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
