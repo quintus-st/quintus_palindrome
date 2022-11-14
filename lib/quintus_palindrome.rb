6d7ea08150e1239837f951650534a5d0f8e3d393
quintus-st <quintus.flannagan@simplethread.com>
2022-11-14 15:08:43 -0500

    refactored palindrome method

--- lib/quintus_palindrome.rb
+++ lib/quintus_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "quintus_palindrome/version"
 
-module QuintusPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
